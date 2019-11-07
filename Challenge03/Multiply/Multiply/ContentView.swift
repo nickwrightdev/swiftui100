2c7d28ef3ac6369c2bb119bfcf85b90de781c51b
Nick Wright <nick@nickwrightdev.com>
2019-11-06 21:53:49 -0800

    challenge 3 edutainment app checkpoint - generate multiplication questions

--- Challenge03/Multiply/Multiply/ContentView.swift
+++ Challenge03/Multiply/Multiply/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct Question {
     var question: String;
-    var answer: String;
+    var answer: Int;
 }
 
 struct ContentView: View {
@@ -19,12 +19,18 @@ struct ContentView: View {
     @State private var maxMultiplier = 6
     @State private var numQuestionsSelection = 1
     
-    @State private var currentQuestion = 0
+    @State private var currentQuestionIndex = 0
     @State private var questionsThisGame = 0
+    @State private var questions = [Question]()
     @State private var isGameOver = false
+    @State private var currentAnswer = ""
     
     let numQuestionsOptionsStr = ["5", "10", "20", "all"]
     let numQuestionsOptions = [5, 10, 20, 0]
+    
+    var currentQuestion: Question? {
+        return questions[currentQuestionIndex]
+    }
     
     var body: some View {
         Group {
@@ -36,10 +42,12 @@ struct ContentView: View {
                 } else {
                     VStack {
                         Text("Multiplying up to \(maxMultiplier)x")
-                        Text("\(currentQuestion + 1) of \(questionsThisGame)")
+                        Text("\(currentQuestionIndex + 1) of \(questionsThisGame)")
+                        Text("\(currentQuestion!.question)")
+                        TextField("Answer: ", text: $currentAnswer)
                         Button("Answer") {
-                            self.currentQuestion += 1
-                            if self.currentQuestion == self.questionsThisGame {
+                            self.currentQuestionIndex += 1
+                            if self.currentQuestionIndex == self.questionsThisGame {
                                 self.isGameOver = true
                             }
                         }
@@ -58,14 +66,33 @@ struct ContentView: View {
                     }
                     .pickerStyle(SegmentedPickerStyle())
                     
-                    Button("Let's Play") {
-                        self.questionsThisGame = self.numQuestionsOptions[self.numQuestionsSelection]
-                        self.isGameActive = true
-                        self.isGameOver = false
-                        self.currentQuestion = 0
-                    }
+                    Button("Let's Play", action: startGame)
                 }
             }
+        }
+    }
+    
+    func startGame() {
+        questionsThisGame = numQuestionsOptions[numQuestionsSelection]
+        createQuestions()
+        isGameActive = true
+        isGameOver = false
+        currentQuestionIndex = 0
+    }
+    
+    func createQuestions() {
+        questions = [Question]()
+        for i in 1...maxMultiplier {
+            for j in 1...maxMultiplier {
+                let questionStr = "\(i) X \(j)"
+                let answer = i * j
+                let elem = Question(question: questionStr, answer: answer)
+                questions.append(elem)
+            }
+        }
+        questions.shuffle()
+        if questionsThisGame == 0 {
+            questionsThisGame = questions.count
         }
     }
 }
